2ffaaced5d9fcf2715176004031119833bfec8d8
faima <faima.mwami@cellulant.io>
2023-09-01 21:24:12 +0300

    remove creds

--- php/process.php
+++ php/process.php
@@ -16,10 +16,10 @@ try {
     //$mail->SMTPDebug = 2; 
                                         // Enable verbose debug output
     $mail->isSMTP();                                            // Set mailer to use SMTP
-    $mail->Host       = 'mail.darusalamproperties.com';  			// Specify main and backup SMTP servers
+    $mail->Host       = '';  			// Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'info@darusalamproperties.com';                     // SMTP username
-    $mail->Password   = 'Darusalam@123';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
